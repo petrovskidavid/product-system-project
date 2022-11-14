adc685d35ee56186f2d952950c33ed6c53b24852
David Petrovski <petrovskidavid1@gmail.com>
2022-11-14 02:01:11 -0600

    Added get fetch and response for products query

--- server/src/server.js
+++ server/src/server.js
@@ -1,16 +1,27 @@
 import express from "express" 
+import cors from "cors"
 import {internalDb, legacyDb} from "./db.js"
 
 // Initializes express server
 const server = express()
 const port = 8800
 
+server.use(cors())
+
 server.listen(port, () => {
     console.log(`Server is online!\nListening to port ${port}`)
 });
 
-server.post("/api/addProduct", (req, res) => {
+server.get("/api/getProducts", (req, res) => {
+    console.log("Recieved GET for getProducts")
 
+    legacyDb.query("SELECT * FROM parts", (err, rows) => {
+        if (err)
+            throw err
+
+        res.send(rows)
+        console.log("Sent response")
+    })
 })
 
 export default server
