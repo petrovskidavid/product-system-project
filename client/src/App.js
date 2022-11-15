5f6c5dc6400586473fb07b8e0687552009098727
David Petrovski <petrovskidavid1@gmail.com>
2022-11-14 18:17:36 -0600

    Added the different routes for the clients

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,18 @@
-import { useEffect, useState } from "react"
-import Axios from "axios";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import LoginPage from "./pages/LoginPage"
+import StorePage from "./pages/StorePage"
 import './App.css';
-import Navbar from "./components/Navbar"
-import ProductCard from "./components/ProductCard"
 
 export default function App() {
-
-  const [productsData, setProductsData] = useState([]) //< Holds the list of all the products
-
-  // Only calls once per render of the component
-  useEffect(() => {
-
-    // Requests a list of all the products in the database
-    Axios.get("http://localhost:8800/api/getProducts").then((res) => {
-      setProductsData(res.data)
-    })
-  }, [])
-
-  // Loops through the data and creates a ProductCard for each product
-  const productCards = productsData.map((product) => {
-    return (
-      <ProductCard 
-        key={product.number}
-        img={product.pictureURL}
-        description={product.description}
-        weight={product.weight}
-        price={product.price}
-        quantity={product.Quantity}
-      />
-    )
-  })
-
   
   return (
     <div className="App">
-      <Navbar />
-      <div className="product-card-container">
-        {productCards}
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/store" element={<StorePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
