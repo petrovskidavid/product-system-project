147333c3a48f6f5a3a64ceb8b2233aae7bedd953
David Petrovski <petrovskidavid1@gmail.com>
2022-11-14 18:17:06 -0600

    Created different pages for app and transfered code from App.js

---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar"
+
+export default function LoginPage() {
+
+    return (
+        <div>
+            <Navbar />
+            <h1>This is the login Page</h1>
+        </div>
+    )
+}
