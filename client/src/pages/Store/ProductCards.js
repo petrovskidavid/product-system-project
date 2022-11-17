aa541768ff70b495259a46ee44b08fd35d069ddf
David Petrovski <petrovskidavid1@gmail.com>
2022-11-17 02:07:43 -0600

    Implemented the search field

--- client/src/pages/Store/ProductCards.js
+++ client/src/pages/Store/ProductCards.js
@@ -6,6 +6,8 @@ import ProductCard from "../../components/ProductCard"
 export default function ProductCards() {
 
     const [productsData, setProductsData] = useState([]) //< Holds the list of all the products
+    const [searchQuery, setSearchQuery] = useState("")   //< Holds the search query provided by the user
+
 
     // Only calls once per render of the component
     useEffect(() => {
@@ -16,8 +18,30 @@ export default function ProductCards() {
         })
     }, [])
 
-    // Loops through the data and creates a ProductCard for each product
-    const productCards = productsData.map((product) => {
+
+    // Updates the search query as the user types
+    const searchProducts = (event) => {
+        setSearchQuery(event.target.value)
+    }
+
+    // Filters through all the products
+    const productCards = productsData.filter((searchResults) => {
+
+        if (searchQuery === "")
+        {
+            // Empty query, returns all the products
+            return searchResults
+
+        } else if (searchResults.description.toLowerCase().includes(searchQuery.toLowerCase())){
+            // Checks to see if any part of the serach query can be found in the products description and returns the matching ones
+            return searchResults
+        }
+
+        return null //< Fixes warnings
+
+    }).map((product) => {
+
+        // Loops through all the above filtered products and creates product cards for each
         return (
             <ProductCard 
                 key={product.number}
@@ -33,7 +57,7 @@ export default function ProductCards() {
 
     return (
         <div className="store">
-            <SearchField placeholder="Search" />
+            <SearchField placeholder="Search" onChange={searchProducts}/>
             <div className="product-cards-container">
                 {productCards}
             </div>
